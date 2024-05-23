f7d2f4f516e919af63870b43aafc20c096906c13
Pooja-Surwase22 <surwasepoojasp@gmail.com>
2024-05-23 22:25:30 +0530

    Create StaticBlock.java

---
+++ StaticBlock.java
@@ -0,0 +1,12 @@
+package com;
+
+public class StaticBlock {
+	static {
+		System.out.println("This is static method . ");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is main method .");
+
+}
+}
