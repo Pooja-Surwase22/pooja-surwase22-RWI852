870adab804d661b494b7105fc74a3fd533596237
Pooja-Surwase22 <surwasepoojasp@gmail.com>
2024-05-29 22:35:25 +0530

    Create Accmod.java

---
+++ Accmod.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class Accmod {
+     public static void main(String[]args)
+     {
+    	 System.out.println(Accmod2.a);
+    	 System.out.println(Accmod2.c);
+    	 System.out.println(Accmod2.d);
+    	// System.out.println(Accmod2.e); private is used inside same class
+     }
+}
