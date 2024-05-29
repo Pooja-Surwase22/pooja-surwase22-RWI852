40684dd9b049dae57b1892123b79668b4993915a
Pooja-Surwase22 <surwasepoojasp@gmail.com>
2024-05-29 22:34:59 +0530

    Create Final.java

---
+++ Final.java
@@ -0,0 +1,15 @@
+class Final extends B
+{
+	public static void main(String[] args) 
+	{
+  /*   a=100;
+	 System.out.println(a); final variable cannot be re initialized */
+	System.out.println(a);
+	}
+   
+}
+class B
+{
+    static final int a=10;
+	
+}
