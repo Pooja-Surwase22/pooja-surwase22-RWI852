29f3030a8a700a9cb0604e08b48183753a221489
Pooja-Surwase22 <surwasepoojasp@gmail.com>
2024-05-29 22:37:40 +0530

    Create Accmod2.java

---
+++ Accmod2.java
@@ -0,0 +1,16 @@
+package Day1;
+
+public class Accmod2 {
+
+	public static int a=10;
+	static char c='a';
+	protected static int d=109;
+	private static int e=0;
+	public static void main(String[]margs)
+	{
+		System.out.println(a);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+	}
+}
