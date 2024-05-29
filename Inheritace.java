cdee2c5988d41b297a432ea857691796fd449521
Pooja-Surwase22 <surwasepoojasp@gmail.com>
2024-05-29 22:30:24 +0530

    Create Inheritace.java

---
+++ Inheritace.java
@@ -0,0 +1,20 @@
+class Inheritance extends B
+{
+	public static void main(String[] args) 
+	{
+		run();
+		Inheritance aa=new Inheritance();
+		aa.swim();
+	}
+}
+class B
+{
+	public static void run()
+	{
+		System.out.println("running");
+	}
+	public void swim()
+	{
+		System.out.println("swimming");
+	}
+}
