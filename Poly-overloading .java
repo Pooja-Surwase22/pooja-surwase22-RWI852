2f8fa9b94c8e0f1d405879496aebf1f99faab3c5
Pooja-Surwase22 <surwasepoojasp@gmail.com>
2024-06-03 15:10:28 +0530

    Update Poly-overloading .java

--- Poly-overloading .java
+++ Poly-overloading .java
@@ -1,14 +1,14 @@
 public class Poly_overloading {
     //method overloading (compile time)
-    public void manvik(int i){
-        System.out.println("Hi i am first manvik ");
+    public void pooja(int i){
+        System.out.println("Hi i am first pooja");
     }
-    public void manvik(){
-        System.out.println("Hi i am Secound manvik ");
+    public void pooja(){
+        System.out.println("Hi i am Secound pooja ");
     }
     public static void main(String[] args) {
         Poly_overloading sc = new Poly_overloading();
-        sc.manvik();
-        sc.manvik(5);
+        sc.pooja();
+        sc.pooja(5);
     }
 }
