032ade093efef5086866ada06aab24356f505c46
Pooja-Surwase22 <surwasepoojasp@gmail.com>
2024-06-06 15:12:40 +0530

    Create StringConcatenationExample .java

---
+++ StringConcatenationExample .java
@@ -0,0 +1,8 @@
+public class StringConcatenationExample {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "World";
+        String s3 = s1 + " " + s2;  // Concatenating strings with a space in between
+        System.out.println(s3);
+    }
+}
