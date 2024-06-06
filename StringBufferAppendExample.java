405194323fb05b9bc00c64591c5fdd292a3e2528
Pooja-Surwase22 <surwasepoojasp@gmail.com>
2024-06-06 15:14:09 +0530

    Create StringBufferAppendExample.java

---
+++ StringBufferAppendExample.java
@@ -0,0 +1,7 @@
+public class StringBufferAppendExample {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello");
+        sb.append(" World");
+        System.out.println(sb);  // Outputs: Hello World
+    }
+}
