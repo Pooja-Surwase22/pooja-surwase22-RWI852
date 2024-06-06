6c25e0dc69a2eb9e2f94f98a53e852f673bac473
Pooja-Surwase22 <surwasepoojasp@gmail.com>
2024-06-06 15:16:20 +0530

    Create StringBuilderAppendExample.java

---
+++ StringBuilderAppendExample.java
@@ -0,0 +1,7 @@
+public class StringBuilderAppendExample {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" World");
+        System.out.println(sb);  // Outputs: Hello World
+    }
+}
