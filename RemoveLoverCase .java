bc1aec811c83a3521f89adf59bd295f15174b4cf
Pooja-Surwase22 <surwasepoojasp@gmail.com>
2024-06-07 12:11:30 +0530

    Create RemoveLoverCase .java

---
+++ RemoveLoverCase .java
@@ -0,0 +1,24 @@
+package removelovercase;
+
+public class RemoveLoverCase {
+
+	public static void main(String[] args) {
+		String str="Hello poo";
+		String str1=" ";
+		for(int i=0;i<str.length();i++)
+		{
+			char c=str.charAt(i);
+			
+			if(Character.isUpperCase(c))
+			{
+				str1=str1+c;
+			}
+		}
+		System.out.println(str1);
+	}
+	
+		// TODO Auto-generated method stub
+
+	}
+
+
