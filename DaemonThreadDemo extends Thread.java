844d295816f0fa9f1b647f225186a16b5fb4c47a
Pooja-Surwase22 <surwasepoojasp@gmail.com>
2024-06-13 15:26:25 +0530

    Create  DaemonThreadDemo extends Thread.java program

---
+++ DaemonThreadDemo extends Thread.java
@@ -0,0 +1,14 @@
+package com.practise;
+
+public class DaemonThreadDemo extends Thread {
+    public void run() {
+        System.out.println("this is child Thread");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("parent thread");
+        DaemonThreadDemo d = new DaemonThreadDemo();
+        // d.setDaemon(false); // Not needed, threads are non-daemon by default
+        d.start();
+    }
+}
